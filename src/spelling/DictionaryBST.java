8706f83b9118e465237d3110027d6018e9c800e1
Deepti Shatagopam <deepti.shatagopam@gmail.com>
2016-02-15 06:57:25 -0800

    Week4 - Part1 - Dictionary Spell Check

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -9,18 +9,26 @@ import java.util.TreeSet;
 public class DictionaryBST implements Dictionary 
 {
    private TreeSet<String> dict;
-	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
-    
+
+    public DictionaryBST() {
+        this(new TreeSet<String>());
+    }
+
+    public DictionaryBST(TreeSet<String> dict) {
+        this.dict = dict;
+    }
+
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+        if (!isWord(word)) {
+            String lowercaseWord = word.toLowerCase();
+            dict.add(lowercaseWord);
+            return true;
+        }
         return false;
     }
 
@@ -28,14 +36,13 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+        String lowercaseStr = s.toLowerCase();
+        return (dict.contains(lowercaseStr));
     }
 
 }
